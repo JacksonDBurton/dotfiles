c2cc815527e2b7cb2131966da628d7045b64edb1
Jack Burton <jdburton0@outlook.com>
2024-08-08 12:27:24 -0400

    Better support for desired tab function

---
+++ .config/nvim/lua/plugins/cmp_luasnip.lua
@@ -0,0 +1,33 @@
+return { -- override nvim-cmp plugin
+  "hrsh7th/nvim-cmp",
+  -- override the options table that is used in the `require("cmp").setup()` call
+  opts = function(_, opts)
+    -- opts parameter is the default options table
+    -- the function is lazy loaded so cmp is able to be required
+    local ls, cmp = require "luasnip", require "cmp"
+
+    local function has_words_before()
+      local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+    end
+
+    if not opts.mapping then opts.mapping = {} end
+    opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      if ls.expand_or_locally_jumpable() then
+        ls.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+    -- modify the mapping part of the table
+    opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+  end,
+}
